2706c8bdccdc7e7cb5c44eec6b4179c230bad9a0
Wlodek Bzyl <matwb@univ.gda.pl>
2012-06-11 16:00:23 +0200

    poprawki

--- mathformulas/client/javascripts/sidebarnav.js
+++ mathformulas/client/javascripts/sidebarnav.js
@@ -1,3 +1,10 @@
+Template.sidebarnav.categories = function() {
+  var categories = MathFormulas.find().map(function(doc) {
+    return doc.category;
+  });
+  return _.unique(categories);
+};
+
 Template.sidebarnav.events = {
   'click .nav-list .category': function(event) {
     var category = $(event.target).text().trim();
@@ -6,8 +13,32 @@ Template.sidebarnav.events = {
     Session.set('selected_category', category);
 
     var li = $(event.target).closest('li');
-    li.closest('ul').find('li').removeClass('active');
+    var ul = li.closest('ul');
+    ul.find('.category').removeClass('active');
+    ul.find('input').css('background-color', 'white');
     li.addClass('active');
+  },
+  'keydown #new-category': function(event) {
+    var esc = event.which == 27,
+        enter = event.which == 13,
+        tab = event.which == 9,
+        element = $(event.target);
+    if (esc) {
+      element.blur();
+    } else if (enter || tab) {
+      event.preventDefault();
+      // save category
+      var category = element.val().trim();
+      if (category !== '' && _.indexOf(Template.sidebarnav.categories(), category) < 0) {
+        alert('Category named "' + category + '" was created');
+        Session.set('selected_category', category);
+        $('.category').removeClass('active');
+        $('li input').css('background-color', '#5bc0de');
+      } else {
+        alert('Category name already taken or empty!');
+      }
+      element.blur();
+    };
   }
 }
 
